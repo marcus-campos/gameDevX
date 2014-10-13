16528265c5beaaf79031caed743973ad4a196033
danilooliveiradeandrade <danilooliveiradeandrade@gmail.com>
2014-10-13 18:49:42 -0300

    primeiros calculos
    chupa. tenta consertar o erro em program.cs... program.cs tem que herdar
    de calculos. nossas tabelas terão que ter valores bem proximos de 1...
    tipo 1,3,  1,5,   0,6

--- GameDevProjextW/GameDevProjextW/Calculos.cs
+++ GameDevProjextW/GameDevProjextW/Calculos.cs
@@ -3,75 +3,113 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*As valores das contas serão aumentado e diminuido com se e senão posteriormente conforme necessidades*/
 namespace GameDevProjextW
 {
     class Calculos
     {
-        int erro;
-        int design;
-        int tecnologia;
-        int grafico;
+        private Int32 erro = 0;
+        private Int32 design = 0;
+        private Int32 tecnologia = 0;
+        private Int32 pesquisa = 0;
 
-        int starGames;
-        int informedgamer;
-        int gameHero;
-        int allGames;
+        private int starGames;
+        private int informedgamer;
+        private int gameHero;
+        private int allGames;
 
-        public void calcula_Tema_Genero_Plataforma_Grafico()
-        {
-            erro = 0;
-            design = 0;
-            tecnologia = 0;
-            grafico = 0;
+        public void calcula_Tema_Genero_Plataforma_grafico(double tema, double genero, double plataforma, double pesquisa)
+        {//Criando o jogo com configurações iniciais
+            for (int i = 0; i < 5; i++)
+            {
+                erro = erro + Convert.ToInt32(tema * genero / plataforma * pesquisa);
+                design = design + Convert.ToInt32(plataforma * genero / tema * pesquisa);
+                tecnologia = tecnologia + Convert.ToInt32(genero * plataforma / pesquisa * tema);
+                pesquisa = pesquisa + Convert.ToInt32(pesquisa * plataforma / genero * tema);
+                System.Threading.Thread.Sleep(3000);
+            }
+
         }
 
-        public void calcula_Tema_Genero_Plataforma_Grafico(int faixaEtaria)
-        {
-            erro = 0;
-            design = 0;
-            tecnologia = 0;
-            grafico = 0;
+        public void calcula_Tema_Genero_Plataforma_grafico(double tema, double genero, double plataforma, double pesquisa, int faixaEtaria)
+        {//Criando o jogo com configurações iniciais com faixa etaria caso já tenha
+            for (int i = 0; i < 5; i++)
+            {
+                erro = erro + Convert.ToInt32((tema * genero / plataforma * pesquisa) + faixaEtaria / 2);
+                design = design + Convert.ToInt32((plataforma * genero / tema * pesquisa) + faixaEtaria / 2);
+                tecnologia = tecnologia + Convert.ToInt32((genero * plataforma / pesquisa * tema) + faixaEtaria / 2);
+                pesquisa = pesquisa + Convert.ToInt32((pesquisa * plataforma / genero * tema) + faixaEtaria / 2);
+                System.Threading.Thread.Sleep(3000);
+            }
+
         }
 
-        public void calcula_Tema_Genero_Plataforma_Grafico(int faixaEtaria, int engine)
-        {
-            erro = 0;
-            design = 0;
-            tecnologia = 0;
-            grafico = 0;
+        public void calcula_Tema_Genero_Plataforma_grafico(double tema, double genero, double plataforma, double pesquisa, int faixaEtaria, int engine)
+        {//Criando o jogo com configurações iniciais com faixa etaria e engine caso ja tenha
+            for (int i = 0; i < 5; i++)
+            {
+                erro = erro + Convert.ToInt32((tema * genero / plataforma * pesquisa) + faixaEtaria / pesquisa);
+                design = design + Convert.ToInt32((plataforma * genero / tema * pesquisa) + faixaEtaria / pesquisa);
+                tecnologia = tecnologia + Convert.ToInt32((genero * plataforma / pesquisa * tema) + faixaEtaria / pesquisa);
+                pesquisa = pesquisa + Convert.ToInt32((pesquisa * plataforma / genero * tema) + faixaEtaria / pesquisa);
+                System.Threading.Thread.Sleep(3000);
+            }
+
         }
 
-        public void calcula_Engine_Jogabilida_Historia()
-        {
-            erro = 0;
-            design = 0;
-            tecnologia = 0;
-            grafico = 0;
+        public void calcula_Engine_Jogabilida_Historia(double engine, double jogabilidade, double historia)
+        {//Ainda criando o jogo de acordo com porcentagem em engine, jogabilidade e historia
+            for (int i = 0; i < 5; i++)
+            {
+                erro = 0;
+                design = 0;
+                tecnologia = 0;
+                pesquisa = 0;
+                System.Threading.Thread.Sleep(3000);
+            }
         }
 
         public void calcula_Dialogo_Design_IA()
-        {
-            erro = 0;
-            design = 0;
-            tecnologia = 0;
-            grafico = 0;
+        {//Ainda criando o jogo de acordo com porcentagem em dialogo, design e IA
+            for (int i = 0; i < 5; i++)
+            {
+                erro = 0;
+                design = 0;
+                tecnologia = 0;
+                pesquisa = 0;
+                System.Threading.Thread.Sleep(3000);
+            }
         }
 
-        public void calcula_DesignMundo_Grafico_Som()
-        {
-            erro = 0;
-            design = 0;
-            tecnologia = 0;
-            grafico = 0;
+        public void calcula_DesignMundo_grafico_Som()
+        {//Ainda criando o jogo de acordo com porcentagem em design do Mundo, tipo de grafico e sons
+            for (int i = 0; i < 5; i++)
+            {
+                erro = 0;
+                design = 0;
+                tecnologia = 0;
+                pesquisa = 0;
+                System.Threading.Thread.Sleep(3000);
+            }
+            calculaAvaliacao();//calcula a avaliação
+            zerarProducao();//zera erro, design e tecnologia ao final dos jogos.
         }
 
-        public void calculaAvaliacao()
+        public void calculaAvaliacao()//Avalia o jogo
         {
             starGames = 0;
             informedgamer = 0;
             gameHero = 0;
             allGames = 0;
         }
+
+        public void zerarProducao() //Isto roda ao final de cada Jogo
+        {
+            erro = 0;
+            design = 0;
+            tecnologia = 0;
+        }
+
+
     }
 }
--- GameDevProjextW/GameDevProjextW/Program.cs
+++ GameDevProjextW/GameDevProjextW/Program.cs
@@ -6,13 +6,13 @@ using System.Windows.Forms;
 
 namespace GameDevProjextW
 {
-    static class Program : Calculos
+    static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
